0c0c491507687da36227fc8c894d9899627c6d15
mickaelgobbo <gobbo599@hotmail.com>
2017-05-05 00:22:43 +0200

    new purchase

--- Export/src/createFile.java
+++ Export/src/createFile.java
@@ -9,7 +9,6 @@ import org.testng.annotations.Test;
 public class createFile {
 	 @Test
 	  public static void main(String[] args) throws IOException {
-	        String text = "Hello world";
 	        BufferedWriter output = null;
 	        try {
 	            File file = new File("output/customer.txt");
@@ -21,7 +20,7 @@ public class createFile {
 		        output.write(i+ ","); 
 		        output.write("Last name" + i + ",");
 		        output.write("First name" + i + ",");
-		        output.write("Email" + i + ","); 
+		        output.write("Email" + i + "@yahoo.fr,"); 
 	            output.write("\r\n");
 	            }
 	        } catch ( IOException e ) {
@@ -31,5 +30,27 @@ public class createFile {
 	            output.close();
 	          }
 	        }
+	        
+	        try {
+	            File file = new File("output/purchase.txt");
+	            output = new BufferedWriter(new FileWriter(file));
+	            output.write("ID,Purchase label,Price,Amount");
+	            output.write("\r\n");
+	            for (int i=0;i<200;i++)
+	            {
+		        output.write(i+ ","); 
+		        output.write("Label" + i + ",");
+		        output.write(i*5 + ",");
+		        output.write("1"+ ","); 
+	            output.write("\r\n");
+	            }
+	        } catch ( IOException e ) {
+	            e.printStackTrace();
+	        } finally {
+	          if ( output != null ) {
+	            output.close();
+	          }
+	        }
+	        
 	    }
 }
