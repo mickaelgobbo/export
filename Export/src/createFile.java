8f4b8de215c1e1b2669b10b5af2ba0301ff625ea
mickaelgobbo <gobbo599@hotmail.com>
2017-05-04 23:16:46 +0200

    output directory added

--- Export/src/createFile.java
+++ Export/src/createFile.java
@@ -1,18 +1,23 @@
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+
 
 public class createFile {
 	  public static void main(String[] args) throws IOException {
-		    Path path = Paths.get("users.txt");
-		    byte[] contents = Files.readAllBytes(path);
-
-		    Path newPath = Paths.get("newUsers.txt");
-		    byte[] newContents = "aaa".getBytes();
-		    Files.write(newPath, contents, StandardOpenOption.CREATE);
-		    Files.write(newPath, newContents, StandardOpenOption.APPEND);
-		  }
+	        String text = "Hello world";
+	        BufferedWriter output = null;
+	        try {
+	            File file = new File("output/example.txt");
+	            output = new BufferedWriter(new FileWriter(file));
+	            output.write(text);
+	        } catch ( IOException e ) {
+	            e.printStackTrace();
+	        } finally {
+	          if ( output != null ) {
+	            output.close();
+	          }
+	        }
+	    }
 }
