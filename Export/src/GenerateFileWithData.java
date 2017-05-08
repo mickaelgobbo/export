5919f00669f74b350d795c3edc3d7aaeb33b7d7d
mickaelgobbo <gobbo599@hotmail.com>
2017-05-09 00:14:50 +0200

    update

--- Export/src/GenerateFileWithData.java
+++ Export/src/GenerateFileWithData.java
@@ -10,7 +10,7 @@ public class GenerateFileWithData {
 	public static void main(String args[]) throws IOException {
 		String numberLineFile;
 		String fileName;
-		String fileSeparator;
+		final String fileSeparator;
 		int numberLine;
 
 		numberLineFile = JOptionPane.showInputDialog("Saisir le nombre de ligne du fichier :");
@@ -18,56 +18,62 @@ public class GenerateFileWithData {
 		fileSeparator = JOptionPane.showInputDialog("Entrer le separateur que vous voulez :");
 		numberLine = Integer.parseInt(numberLineFile);
 
-        BufferedWriter output = null;
-        try {
-            File file = new File("output/" + fileName + ".txt");
-            output = new BufferedWriter(new FileWriter(file));
-            output.write("ID,Last name,First Name,Email,Gender");
-            output.write("\r\n");
-            for (int i=0;i<numberLine;i++)
-            {
-	        output.write(i+ ","); 
-	        output.write("Last name" + i + ",");
-	        output.write("First name" + generateRandomString(8) + ",");
-	        output.write("Email" + i + "@yahoo.fr,"); 
-	        //O unknown, 1 Male and 2 female
-	        output.write(generateRandomNumber(2)); 
-            output.write("\r\n");
-            }
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        } finally {
-          if ( output != null ) {
-            output.close();
-          }
-        }
-        
-        try {
-            File file = new File("output/purchase.txt");
-            output = new BufferedWriter(new FileWriter(file));
-            output.write("ID,Purchase label,Price,Amount");
-            output.write("\r\n");
-            for (int i=0;i<200;i++)
-            {
-	        output.write(i+ ","); 
-	        output.write("Label" + i + ",");
-	        output.write(i*5 + ",");
-	        output.write("1"+ ","); 
-            output.write("\r\n");
-            }
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        } finally {
-          if ( output != null ) {
-            output.close();
-          }
-        }
+		BufferedWriter output = null;
+		try {
+			File file = new File("output/" + fileName + ".txt");
+			output = new BufferedWriter(new FileWriter(file));
+			if (fileSeparator.isEmpty()) {
+				output.write("ID,Last name,First Name,Email,Gender");
+			} else {
+				output.write(replaceStringWithSeparator("ID,Last name,First Name,Email,Gender", fileSeparator));
+			}
+			output.write("\r\n");
+			for (int i = 0; i < numberLine; i++) {
+				//output.write(i + ",");
+				output.write(i);
+				output.write(",");
+				//addSeparator(output,fileSeparator);
+				output.write("Last name" + i + ",");
+				output.write("First name" + generateRandomString(8) + ",");
+				output.write("Email" + i + "@yahoo.fr,");
+				// O unknown, 1 Male and 2 female
+				output.write(generateRandomNumber(2));
+				output.write("\r\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (output != null) {
+				output.close();
+			}
+		}
+
+		try {
+			File file = new File("output/purchase.txt");
+			output = new BufferedWriter(new FileWriter(file));
+			output.write("ID,Purchase label,Price,Amount");
+			output.write("\r\n");
+			for (int i = 0; i < 200; i++) {
+				output.write(i + ",");
+				output.write("Label" + i + ",");
+				output.write(i * 5 + ",");
+				output.write("1" + ",");
+				output.write("\r\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (output != null) {
+				output.close();
+			}
+		}
 
 		System.exit(0);
 	}
 
 	/**
 	 * Generation of random number
+	 * 
 	 * @param in_valeurMax
 	 * @return
 	 */
@@ -75,23 +81,47 @@ public class GenerateFileWithData {
 		Random r = new Random();
 		return r.nextInt(in_valeurMax);
 	}
-	
+
 	/**
 	 * Generate a random string
+	 * 
 	 * @param characters
 	 * @param length
 	 * @return
 	 */
-	private static String generateRandomString(int length)
-	{
-		String characters= "abcdefghijklmnopqrstuvwxyz";
-	    char[] text = new char[length];
-	    for (int i = 0; i < length; i++)
-	    {
+	private static String generateRandomString(int length) {
+		String characters = "abcdefghijklmnopqrstuvwxyz";
+		char[] text = new char[length];
+		for (int y = 0; y < length; y++) {
 			Random rng = new Random();
-	        text[i] = characters.charAt(rng.nextInt(characters.length()));
-	    }
-	    return new String(text);
+			text[y] = characters.charAt(rng.nextInt(characters.length()));
+		}
+		return new String(text);
 	}
-     
+
+	/**
+	 * Thie function replace in the string a value with the right separator
+	 * 
+	 * @param in_value
+	 * @param in_separator
+	 * @return
+	 */
+	private static String replaceStringWithSeparator(String in_value, String in_separator) {
+		return in_value.replace(",", in_separator);
+	}
+	
+	/**
+	 * Add the right separator in the file
+	 * @param File
+	 * @param in_fileSeparator
+	 * @throws IOException
+	 */
+	private static void addSeparator(BufferedWriter File,String in_fileSeparator) throws IOException {
+		if (in_fileSeparator.isEmpty()) {
+			File.write(",");
+		} else {
+			File.write(in_fileSeparator);
+		}
+	}	
+	
 }
